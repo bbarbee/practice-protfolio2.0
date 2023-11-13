bcdf801defbee600327c06e6ddf41a0bf9caef86
Brooke Barbee <109841386+bbarbee@users.noreply.github.com>
2023-11-13 16:18:37 -0600

    updates
    added more css, corrected the navbar to extend across the page, added a temp image where id like a logo

--- script.js
+++ script.js
@@ -7,9 +7,24 @@ const subject= document.getElementById('subject').value;
 
 
 function submitForm(){
-    if (!firstName || !lastName || !eamil || !subject){
+    if (!firstName || !lastName || !email || !subject){
         alert('Please fill in all fields');
         return;
     }
 
+    const emailBody= `Name: ${firstName} ${lastName}\nEmail: ${email}\nSubject: ${subject}`;
+
+    alert(emailBody, 'Form submitted sucessfully');
+
+    document.getElementById('contactForm').reset();
+
+}
+
+function navBar() {
+    let navigation = document.getElementById("topNav");
+    if (navigation.className === "navbar") {
+        navigation.className += "responsive";
+    } else {
+        navigation.className = "navbar";
+    }
 }
